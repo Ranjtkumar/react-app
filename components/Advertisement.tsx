156af3a7f06d76ba88ef180ad4ee739a793c8c75
unknown <ranjitkumarm5427@gmail.com>
2024-02-01 20:42:45 +0530

    changes

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -8,7 +8,15 @@ const Advertisement : React.FC<{}> = ({})=>{
         return(
             <div>
                 <img src="/assets/school.jpg" className="w-full"/>
+
+                <div className="grid grid-cols-12">
+                    <div className="flex">
+                        
+                    </div>
+                </div>
             </div>
+
+
         )
 }
 
